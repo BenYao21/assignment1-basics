b25b2f978342b58005ed5049863121b8dc0d0dd5
BenYao21 <cyao22@asu.edu>
2025-08-02 18:19:04 -0400

    bpe train finished

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,65 +1,59 @@
 import regex as re
-from collections import Counter
+import collections
 
-def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    test_input = f'''
-    low low low low low
-    lower lower widest widest widest
-    newest newest newest newest newest newest
-    '''
-    new_words = re.findall(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", test_input)
-    word_freqs = Counter()
-    for word in new_words:
-        word_freqs[word] += 1
-    alphabet = []
-    for w in word_freqs.keys():
-        for c in w:
-            if c not in alphabet:
-                alphabet.append(c)
-    alphabet.sort()
+def train(data: str, pat_str: str, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    #ranks: bytes -> int
+    ranks = {}
+    #vocab: int -> bytes
+    vocab = {}
+    for i in range(2**8):
+        ranks[bytes([i])] = i
+        vocab[i] = bytes([i])
+    #merges: list of tuples of bytes
+    merges = []
 
-    vocabs = ["<|endoftext|>"] + alphabet.copy()
-    splits = {word: [c for c in word] for word in word_freqs.keys()}
-    def compute_pair_freqs(splits: dict[str, list[str]], word_freqs: Counter) -> dict[tuple[str, str], int]:
-        pair_freqs = Counter()
-        for word, freq in word_freqs.items():
-            split = splits[word]
-            if len(split) == 1:
-                continue
-            for i in range(len(split) - 1):
-                pair = (split[i], split[i + 1])
-                pair_freqs[pair] += freq
-        return pair_freqs
-    pair_freqs = compute_pair_freqs(splits, word_freqs)
-    best_pair = pair_freqs.most_common(1)[0]
-    def merge_pair(a, b, splits: dict[str, list[str]]):
-        for word in splits.keys():
-            split = splits[word]
-            if len(split) == 1:
-                continue
+    words: list[list[bytes]] = [
+        [bytes([b]) for b in word.encode('utf-8')] for word in re.findall(pat_str, data)
+    ]
+
+    while len(ranks) < vocab_size:
+        stats = collections.Counter()
+        for piece in words:
+            for pair in zip(piece[:-1],piece[1:]):
+                stats[pair] += 1
+
+        #most_common_pair is a tuple of bytes, compare its count and lexicographic order
+        most_common_pair = max(stats, key=lambda x: (stats[x], x))
+        token_bytes = most_common_pair[0]
+        token = len(ranks)
+        #Add the new token
+        ranks[token_bytes] = token
+        vocab[token] = token_bytes
+        merges.append(most_common_pair)
+        new_words = []
+        for word in words:
+            new_word = []
             i = 0
-            while i < len(split) - 1:
-                if split[i] == a and split[i + 1] == b:
-                    split = split[:i] + [a + b] + split[i + 2:]
-                    splits[word] = split
+            while i < len(word) - 1:
+                if (word[i], word[i+1]) == most_common_pair:
+                    new_word.append(token_bytes)
+                    i += 2
                 else:
+                    new_word.append(word[i])
                     i += 1
-            splits[word] = split
-        return splits
-    merges = {}
-    while len(vocabs) < vocab_size:
-        pair_freqs = compute_pair_freqs(splits, word_freqs)
-        best_pair = ""
-        max_freq = None
-        for pair, freq in pair_freqs.items():
-            if max_freq is None or freq > max_freq:
-                best_pair = pair
-                max_freq = freq
-        splits = merge_pair(best_pair[0], best_pair[1], splits)
-        merges[best_pair] = best_pair[0] + best_pair[1]
-        vocabs.append(best_pair[0] + best_pair[1])
-        print(best_pair)
-    print(merges)
+            if i == len(word) - 1:
+                new_word.append(word[i])
+            new_words.append(new_word)
+        words = new_words
 
+    return vocab, merges
 
-train_bpe("test.txt", 20, [], {})
+def bpe_train(input_path: str, vocab_size: int, special_tokens: list[str]):
+    pattern = (
+
+    )
+    with open(input_path) as f:
+        data = f.read()
+
+    vocab, merges = train(data, pattern, vocab_size, special_tokens)
+    return vocab, merges
