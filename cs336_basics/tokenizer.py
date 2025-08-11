575250778b4f6703b3999df43589bde8c9230d82
BenYao21 <cyao22@asu.edu>
2025-08-10 22:47:51 -0400

    first attempt

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -2,8 +2,38 @@ from typing import Iterable, Iterator
 import regex as re
 import json
 
-def get_chunks(text: str, max_length: int) -> list[str]:
-    pass
+def get_chunks(text: str) -> list[str]:
+    PAT = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+    return PAT.findall(text)
+
+def apply_merges(word_bytes: bytes, merge_set: set, vocab_to_idx: dict[bytes, int]) -> tuple[bytes, ...]:
+    word_bytes = list(word_bytes)
+    while True:
+        min_token_id = float('inf')
+        best_pair_id = -1
+        merged = None
+        for i in range(len(word_bytes) - 1):
+            pair = tuple(word_bytes[i:i+2])
+            if pair in merge_set:
+                combined = pair[0] + pair[1]
+                token_id = vocab_to_idx[combined]
+                if token_id is not None and token_id < min_token_id:
+                    min_token_id = token_id
+                    best_pair_id = i
+                    merged = pair
+        if best_pair_id == -1:
+            break
+        word_bytes = (word_bytes[:best_pair_id] + [vocab_to_idx[combined]] + word_bytes[best_pair_id+2:])
+    return tuple(word_bytes)
+
+def encode_merged(text: str, merges: list, vocab_to_idx: dict[bytes, int]) -> list[int]:
+    word_list = get_chunks(text)
+    tokens = []
+    for word in word_list:
+        word_bytes = word.encode("utf-8")
+        word_bytes = apply_merges(word_bytes, merges, vocab_to_idx)
+        tokens.extend(vocab_to_idx[i] for i in word_bytes)
+    return tokens
 
 class Tokenizer:
     def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens = None):
